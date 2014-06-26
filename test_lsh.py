2a5d3f5c8805d4769bdda0f3f2688e420607c615
Siping Ji <hotcoderj@gmail.com>
2014-06-25 23:01:59 -0700

    add index and retrieval using banding algorithm

--- test_lsh.py
+++ test_lsh.py
@@ -25,7 +25,7 @@ def test_minhash(feat_dim=5, sig_dim=1000):
     print "jaccard similarity: " +\
      str(sim.compute_similarity(x.feature, y.feature))
     print "approximated similarity: " +\
-     str(sim.approximate_similarity(x.siganuture, y.siganuture))
+     str(sim.approximate_similarity(x.signature, y.signature))
 
 def test_random_projection(feat_dim=5, sig_dim=1000):
     ''' test lsh for cosine similarity'''
@@ -42,11 +42,25 @@ def test_random_projection(feat_dim=5, sig_dim=1000):
     print "cosine similarity: " + \
         str(sim.compute_similarity(x.feature, y.feature))
     print "approximated similarity: " + \
-        str(sim.approximate_similarity(x.siganuture, y.siganuture))
+        str(sim.approximate_similarity(x.signature, y.signature))
 
+def test_index_retrieval(feat_dim=10, sig_dim=100):
+    '''Test lsh index and retrieval'''
+    sim = lsh.JaccardSimilarity()
+    lshash = lsh.Lsh(feat_dim, sig_dim, sim)
+    for _ in xrange(100):
+        lshash.index(lsh.LshObject(gen_random_bit_vector(feat_dim)))
+
+    for _ in xrange(5):
+        obj = lsh.LshObject(gen_random_bit_vector(feat_dim))
+        results = lshash.retrieve(obj)
+        print "Data retrieved for %s:" % obj
+        for result in results:
+            print result
 
 if __name__ == '__main__':
     test_random_projection()
     test_minhash()
+    test_index_retrieval()
 
 
